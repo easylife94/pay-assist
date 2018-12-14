790079c2565e33e79c96fddb469fd7f3fa508cbe
chenwei <chenwei@qq.com>
2018-12-14 08:53:14 +0800

    x

--- pay-assist-web/src/main/java/com/pay/assist/web/PayAssistWebApplication.java
+++ pay-assist-web/src/main/java/com/pay/assist/web/PayAssistWebApplication.java
@@ -11,7 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableEurekaServer
 @EnableScheduling
-@Enable
 public class PayAssistWebApplication {
 
     public static void main(String[] args) {
